cdf1967637e3ebfc0d8835931b2b508ced9cd9d7
joey2262 <connect@josephdechant.com>
2019-05-26 15:14:06 -0600

    hover hamburger nav working properly

--- themes/glacier-child/assets/js/custom-scripts.js
+++ themes/glacier-child/assets/js/custom-scripts.js
@@ -4,25 +4,31 @@ jQuery(document).ready(function($){
 
 var nav = document.querySelector(".main-navigation");
 var navActive = false;
-var windowActive = false;
+var navHover = false;
+var windowActive = true;
 var html = document.querySelector("html");
+var htmlInTimeout;
+
 
 console.log(nav);
 
 nav.addEventListener("mouseover", function(){
+    navHover = true;
     if(!navActive){
         navActive = true;
         console.log(navActive);
         $(".main-navigation").addClass("active");
     }
+
 });
 
 nav.addEventListener("mouseleave", function(){
     setTimeout(function(){
+        navHover = false;
         if(windowActive){
             if(navActive){
                 navActive = false;
-                console.log(navActive);
+                console.log("nav mouseleave ");
                 $(".main-navigation").removeClass("active");
 
             }
@@ -32,16 +38,23 @@ nav.addEventListener("mouseleave", function(){
 html.addEventListener("mouseleave", function(){
     console.log("OUT");
     windowActive = false;
+    navHover = false;
 });
-html.addEventListener("mouseover", function(){
-    console.log("IN");
-    if(windowActive){
-
-        setTimeout(function(){
-            
+html.addEventListener("mouseenter", function(){
+    clearTimeout(htmlInTimeout);
+    if(!windowActive && navActive){
+        console.log("IN");
+        console.log("navActive " +navActive)
+        console.log("navHover " +navHover)
+        htmlInTimeout = setTimeout(function(){
             windowActive = true;
+            if(navActive && !navHover){
+                navActive = false;
+                console.log("html mouseover ");
+                $(".main-navigation").removeClass("active");
+            }
+        },150);
+    }
             
-        });
-    },50);
-
+    });
 });
