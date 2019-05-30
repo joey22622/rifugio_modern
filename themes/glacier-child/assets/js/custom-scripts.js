909ba7ef4614b6cb6baa489b006d3e0eb0940c41
joey2262 <connect@josephdechant.com>
2019-05-30 17:11:24 -0600

    cleanup of stylesheets, js.  mobile nav working, minor bug fixes, styling still required

--- themes/glacier-child/assets/js/custom-scripts.js
+++ themes/glacier-child/assets/js/custom-scripts.js
@@ -12,95 +12,123 @@ var htmlInTimeout;
 
 console.log(nav);
 $(window).on("scroll", function(){
-    // navHover = false;
-    // if(windowActive){
-        if(navActive){
-            navActive = false;
-            console.log("nav mouseleave ");
-            $(".main-navigation").removeClass("active");
-
-        }
-    // }
+    if(navActive){
+        navActive = false;
+        console.log("nav mouseleave ");
+        $(".main-navigation, .main-navigation li").removeClass("active");
+    }
 });
 
 nav.addEventListener("mouseover", function(){
-    navHover = true;
-    if(!navActive){
-        navActive = true;
-        console.log(navActive);
-        $(".main-navigation").addClass("active");
+    if($("body").hasClass("hasHover")){
+        navHover = true;
+        if(!navActive){
+            navActive = true;
+            console.log(navActive);
+            $(".main-navigation").addClass("active");
+        }
     }
-
 });
 
 nav.addEventListener("mouseleave", function(){
-    setTimeout(function(){
-        navHover = false;
-        if(windowActive){
-            if(navActive){
-                navActive = false;
-                console.log("nav mouseleave ");
-                $(".main-navigation").removeClass("active");
+    if($("body").hasClass("hasHover")){
+        setTimeout(function(){
+            navHover = false;
+            if(windowActive){
+                if(navActive){
+                    navActive = false;
+                    console.log("nav mouseleave ");
+                    $(".main-navigation").removeClass("active");
 
+                }
             }
-        }
-    },50);
+        },50);
+    }
 });
 html.addEventListener("mouseleave", function(){
-    console.log("OUT");
-    windowActive = false;
-    navHover = false;
+    if($("body").hasClass("hasHover")){
+        console.log("OUT");
+        windowActive = false;
+        navHover = false;
+    }
 });
 html.addEventListener("mouseenter", function(){
-    clearTimeout(htmlInTimeout);
-    if(!windowActive && navActive){
-        console.log("IN");
-        console.log("navActive " +navActive)
-        console.log("navHover " +navHover)
-        htmlInTimeout = setTimeout(function(){
-            windowActive = true;
-            if(navActive && !navHover){
-                navActive = false;
-                console.log("html mouseover ");
-                $(".main-navigation").removeClass("active");
-            }
-        },150);
+    if($("body").hasClass("hasHover")){
+        clearTimeout(htmlInTimeout);
+        if(!windowActive && navActive){
+            console.log("IN");
+            console.log("navActive " +navActive)
+            console.log("navHover " +navHover)
+            htmlInTimeout = setTimeout(function(){
+                windowActive = true;
+                if(navActive && !navHover){
+                    navActive = false;
+                    console.log("html mouseover ");
+                    $(".main-navigation .active").removeClass("active");
+                }
+            },150);
+        }
+    }      
+});
+function toggleSubMenu(){
+    //checks if screen hasHover.. if so:
+        //checks if subMenu has class
+console.log("toggleSubMenu");
+    if($("body").hasClass("hasHover")){
+        console.log("if");
+
+    } else {
+        console.log("else");
+
+        $(this).parent().addClass("active");
     }
-            
-    });
+}
 
+$(".menu-item-has-children>a").on("click", toggleSubMenu);
+$(".hamburger").on("click", function(){
+    if(!$("body").hasClass("hasHover")){
+        if($(".main-navigation").hasClass("active")){
+            navActive = false;
+            $(".main-navigation, li").removeClass("active");
+        } else {
+            $(".main-navigation").addClass("active");
+            navActive = true;
+        }
+    }
 
-    function watchForHover() {
-		var hasHoverClass = false;
-		var container = document.body;
-		var lastTouchTime = 0;
-	
-		function enableHover() {
-			// filter emulated events coming from touch events
-			if (new Date() - lastTouchTime < 500) return;
-			if (hasHoverClass) return;
-	
-			container.className += ' hasHover';
-			hasHoverClass = true;
-		}
-	
-		function disableHover() {
-			if (!hasHoverClass) return;
-	
-			container.className = container.className.replace(' hasHover', '');
-			hasHoverClass = false;
-		}
-	
-		function updateLastTouchTime() {
-			lastTouchTime = new Date();
-		}
-	
-		document.addEventListener('touchstart', updateLastTouchTime, true);
-		document.addEventListener('touchstart', disableHover, true);
-		document.addEventListener('mousemove', enableHover, true);
-	
-		enableHover();
-	}
+});
+function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.body;
+    var lastTouchTime = 0;
 
-	watchForHover();
+    function enableHover() {
+        // filter emulated events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
+
 });
