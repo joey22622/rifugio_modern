c87df3ed7bebcda6adcae1f6d58acc8c84b9acc5
joey2262 <connect@josephdechant.com>
2019-06-11 01:39:22 -0600

    modifications to bootstrap grid on portfolio-item page

---
+++ themes/glacier-child/single-portfolio.1.php
@@ -0,0 +1,231 @@
+<?php
+
+ /*
+ * Name: Single Project Portfolio
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<?php if ( class_exists('acf') ) : ?>
+
+	<?php
+
+	// vars ( ACF )
+	$images = get_field('gallery_projects');
+	$visible = get_field('visible_details');
+	$variants = get_field('single_variants_style');
+	// $column_single = get_field('column_single');
+	// $social_icons_portfolio = get_field('social_icons_portfolio');
+
+	// project description
+	$project_title = get_field('project_details');
+	// $author_title = get_field('author_title');
+	// $author = get_field('author');
+	// $client_title = get_field('client_name_title');
+	// $project_date_title = get_field('project_date_title');
+	// $project_location_title = get_field('project_location_title');
+	// $client_name = get_field('client_name');
+	// $project_date = get_field('project_date');
+	$project_location = get_field('project_location');
+
+	// parallax settings
+	// $parallax_image = get_field('parallax_image');
+	// $parallax_header = get_field('parallax_header');
+	// $title_parallax = get_field('title_parallax');
+	// $text_animation_parallax = get_field('text_animation_parallax');
+	// $background_video_parallax = get_field('background_video_parallax');
+	// $youtube_parallax = get_field('youtube_parallax');
+	// $vimeo_parallax = get_field('vimeo_parallax');
+	// $self_hosted_parallax = get_field('self_hosted_parallax');
+	
+	?>
+
+	<?php if ( $parallax_header == 'show' ) : ?>
+
+			<div class="parallax" <?php if ( $background_video_parallax == 'youtube' || $background_video_parallax == 'vimeo' ) : ?>
+			 data-jarallax-video="<?php echo esc_url($youtube_parallax); ?>" <?php endif; ?> <?php if ( $background_video_parallax == 'html5' ) : ?> data-jarallax-video="mp4:<?php echo esc_url($self_hosted_parallax); ?>" <?php endif; ?> style="background-image: url(<?php echo esc_url($parallax_image); ?>)">
+			    <div class="info">
+				     <h2 data-in-effect="<?php echo esc_html($text_animation_parallax); ?>"><?php echo esc_html($title_parallax); ?></h2>
+			    </div>
+			</div>
+
+	<?php endif; ?> 
+
+        
+    <div class="container">
+
+      <div class="row">
+					<?php if ( $variants == 'three' ) : 
+						$column_single = 'col-md-4'?> 
+
+      		<!-- VARIANT PROJECT 3 -->
+			<div class="col-md-12">
+				<div  class="row post-title-row">
+					<h1><?php echo get_the_title(); ?></h1>
+				</div>
+					<?php if ( $column_single == 'col-md-6' || $column_single == 'col-md-4' || $column_single == 'col-md-3') : ?> 
+
+						<div class="row">
+
+					<?php endif; ?>
+
+				<?php if ( $images ) : ?> 
+
+				        <?php foreach ( $images as $image ) : ?>
+				            <div class="gallery-projects <?php echo esc_html($column_single); ?>">
+				             <div class="overlay-box">
+				                <a href="<?php echo esc_url($image['url']); ?>" title="<?php echo esc_html($image['title']); ?>" class="showcase" data-rel="lightcase:gallery">
+									 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_html($image['alt']); ?>">
+				                </a>
+				             </div>	
+				            </div>
+				        <?php endforeach; ?>
+
+				<?php endif; ?>
+
+					<?php if ( $column_single == 'col-md-6' || $column_single == 'col-md-4' || $column_single == 'col-md-3') : ?> 
+
+					</div>
+
+					<?php endif; ?>
+
+			</div>
+			<!-- END VARIANT PROJECT 3 -->
+
+			<?php endif; ?>
+			
+       		
+					<?php if ( $variants == 'one' ) : 
+						$column_single = 'col-md-6'?> 
+
+      		<!-- VARIANT PROJECT 1 -->
+			<div class="col-md-7">
+			<?php if ( $images ) : ?> 
+			        <?php foreach ( $images as $image ) : ?>
+			            <div class="gallery-projects <?php echo esc_html($column_single); ?>">
+			               <div class="overlay-box">
+			                <a href="<?php echo esc_url($image['url']); ?>" title="<?php echo esc_html($image['title']); ?>" class="showcase" data-rel="lightcase:gallery">
+								 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_html($image['alt']); ?>">
+			                </a>
+			             </div>
+			            </div>
+			        <?php endforeach; ?>
+			<?php endif; ?>
+
+			</div>
+			<!-- END VARIANT PROJECT 1 -->
+
+			<?php endif; ?>
+		
+
+			<?php if ( $variants == 'one' || $variants == 'two') : ?> 
+
+      		<div class="col-md-5 single-project">
+					<div  class="row post-title-row">
+					<h1><?php echo get_the_title(); ?></h1>
+				</div>
+
+      		<?php endif; ?>
+
+      		<?php if ( $variants == 'three' || $variants == 'four') : ?> 
+
+      		<div class="col-md-12 single-project">
+
+      		<?php endif; ?>
+
+
+
+
+			<?php 
+
+				while ( have_posts() ) : the_post(); 
+
+					the_content(); 
+					
+				endwhile;
+				 
+			?>	
+
+
+				<?php if ( $visible == 'show') : ?> 
+
+
+
+				<div class="project-details">
+
+				<!-- <h4><?php echo esc_html($project_title); ?></h4> -->
+
+				<h5><?php echo esc_html($author_title); ?></h5>
+					<p><?php echo esc_html($author); ?></p>
+
+				<h5><?php echo esc_html($client_title); ?></h5>
+					<p><?php echo esc_html($client_name); ?></p>
+
+				<h5><?php echo esc_html($project_date_title); ?></h5>
+					<p><?php echo esc_html($project_date); ?></p>
+
+				<h5><?php echo esc_html($project_location_title); ?></h5>
+					<p><?php echo esc_html($project_location); ?></p>
+
+<?php
+// check if the repeater field has rows of data
+						if( have_rows('project_brands') ):	
+?>
+				<p class="project-brands">
+<?php
+							$rowCount = count( get_field('project_brands')); //GET THE COUNT
+							$i = 1;
+							$commaSpace = ", ";
+							 while ( have_rows('project_brands') ) : the_row();
+								 $post_object = get_sub_field('brand');
+									 if($i >= $rowCount):
+										$commaSpace = "";
+									 endif;
+									if($post_object):
+										$post = $post_object;
+										setup_postdata( $post );
+?>
+<a href="<?php the_permalink()?>"><?php echo  get_the_title(); echo $commaSpace; ?></a>
+<?php						
+								wp_reset_postdata();
+								endif;
+								$i++;
+							endwhile;
+?>
+				</p>
+				<!-- /.project-brands -->
+<?php
+						endif;
+
+
+							if ( $social_icons_portfolio == 'show' ) : ?> 
+
+					<h5><?php esc_html_e('Share', 'glacier' ); ?></h5>
+					<?php get_template_part('template-parts/social-share'); ?>
+
+				<?php endif; ?>
+
+				</div>
+
+				<?php endif; ?>
+
+			</div>
+			
+
+		 </div>
+
+		 <!-- NAVIGATION -->
+		 <div class="portfolio-single-nav">
+
+			<?php echo glacier_single_navigation(); ?>
+
+		 </div>
+		 <!-- END NAVIGATION -->
+
+		</div>
+
+<?php endif; ?>
+
+<?php get_footer();
--- themes/glacier-child/single-portfolio.php
+++ themes/glacier-child/single-portfolio.php
@@ -11,171 +11,81 @@
 <?php if ( class_exists('acf') ) : ?>
 
 	<?php
-
 	// vars ( ACF )
 	$images = get_field('gallery_projects');
+	$logo = get_field('brand_logo');
+	$website = get_field('brand_link');
 	$visible = get_field('visible_details');
-	$variants = get_field('single_variants_style');
-	// $column_single = get_field('column_single');
-	// $social_icons_portfolio = get_field('social_icons_portfolio');
-
-	// project description
+	$variants = "three";
 	$project_title = get_field('project_details');
-	// $author_title = get_field('author_title');
-	// $author = get_field('author');
-	// $client_title = get_field('client_name_title');
-	// $project_date_title = get_field('project_date_title');
-	// $project_location_title = get_field('project_location_title');
-	// $client_name = get_field('client_name');
-	// $project_date = get_field('project_date');
 	$project_location = get_field('project_location');
-
-	// parallax settings
-	// $parallax_image = get_field('parallax_image');
-	// $parallax_header = get_field('parallax_header');
-	// $title_parallax = get_field('title_parallax');
-	// $text_animation_parallax = get_field('text_animation_parallax');
-	// $background_video_parallax = get_field('background_video_parallax');
-	// $youtube_parallax = get_field('youtube_parallax');
-	// $vimeo_parallax = get_field('vimeo_parallax');
-	// $self_hosted_parallax = get_field('self_hosted_parallax');
-	
 	?>
 
-	<?php if ( $parallax_header == 'show' ) : ?>
-
-			<div class="parallax" <?php if ( $background_video_parallax == 'youtube' || $background_video_parallax == 'vimeo' ) : ?>
-			 data-jarallax-video="<?php echo esc_url($youtube_parallax); ?>" <?php endif; ?> <?php if ( $background_video_parallax == 'html5' ) : ?> data-jarallax-video="mp4:<?php echo esc_url($self_hosted_parallax); ?>" <?php endif; ?> style="background-image: url(<?php echo esc_url($parallax_image); ?>)">
-			    <div class="info">
-				     <h2 data-in-effect="<?php echo esc_html($text_animation_parallax); ?>"><?php echo esc_html($title_parallax); ?></h2>
-			    </div>
-			</div>
-
-	<?php endif; ?> 
-
-        
     <div class="container">
-
-      <div class="row">
-					<?php if ( $variants == 'three' ) : 
-						$column_single = 'col-md-4'?> 
-
-      		<!-- VARIANT PROJECT 3 -->
+    	<div class="row">
 			<div class="col-md-12">
+				<div class="container">
 				<div  class="row post-title-row">
-					<h1><?php echo get_the_title(); ?></h1>
-				</div>
-					<?php if ( $column_single == 'col-md-6' || $column_single == 'col-md-4' || $column_single == 'col-md-3') : ?> 
+					<div class="col-md-12 title-inner-wrap">
+						<h1><?php echo get_the_title(); ?></h1>
+	
+	<?php if($logo): ?>
 
-						<div class="row">
+						<div class="logo-wrap"><?php echo $logo; ?></div><!-- /.logo-wrap -->
 
-					<?php endif; ?>
-
-				<?php if ( $images ) : ?> 
-
-				        <?php foreach ( $images as $image ) : ?>
-				            <div class="gallery-projects <?php echo esc_html($column_single); ?>">
-				             <div class="overlay-box">
-				                <a href="<?php echo esc_url($image['url']); ?>" title="<?php echo esc_html($image['title']); ?>" class="showcase" data-rel="lightcase:gallery">
-									 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_html($image['alt']); ?>">
-				                </a>
-				             </div>	
-				            </div>
-				        <?php endforeach; ?>
-
-				<?php endif; ?>
-
-					<?php if ( $column_single == 'col-md-6' || $column_single == 'col-md-4' || $column_single == 'col-md-3') : ?> 
-
+	<?php endif; ?>
 					</div>
+					<!-- /.col-md-12.title-inner-wrap -->
 
-					<?php endif; ?>
-
-			</div>
-			<!-- END VARIANT PROJECT 3 -->
-
-			<?php endif; ?>
-			
-       		
-					<?php if ( $variants == 'one' ) : 
-						$column_single = 'col-md-6'?> 
-
-      		<!-- VARIANT PROJECT 1 -->
-			<div class="col-md-7">
-			<?php if ( $images ) : ?> 
-			        <?php foreach ( $images as $image ) : ?>
-			            <div class="gallery-projects <?php echo esc_html($column_single); ?>">
-			               <div class="overlay-box">
-			                <a href="<?php echo esc_url($image['url']); ?>" title="<?php echo esc_html($image['title']); ?>" class="showcase" data-rel="lightcase:gallery">
-								 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_html($image['alt']); ?>">
-			                </a>
-			             </div>
-			            </div>
-			        <?php endforeach; ?>
-			<?php endif; ?>
-
-			</div>
-			<!-- END VARIANT PROJECT 1 -->
-
-			<?php endif; ?>
-		
-
-			<?php if ( $variants == 'one' || $variants == 'two') : ?> 
-
-      		<div class="col-md-5 single-project">
-					<div  class="row post-title-row">
-					<h1><?php echo get_the_title(); ?></h1>
 				</div>
+				<!-- /.row.post-title-row -->
 
-      		<?php endif; ?>
+				<div class="row content-wrap">
+	<?php if ( $images ) : ?> 
+				<div class="row">
+		<?php foreach ( $images as $image ) : ?>
+					<div class="col-md-4 gallery-projects <?php echo esc_html($column_single); ?>">
+						<a href="<?php echo esc_url($image['url']); ?>" title="<?php echo esc_html($image['title']); ?>" class="showcase" data-rel="lightcase:gallery">
+								<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_html($image['alt']); ?>">
+						</a>
+					</div>
+					<!-- /.col-md-12.gallery-projects -->
+		<?php endforeach; ?>
+				</div>
+				<!-- /.row -->
 
-      		<?php if ( $variants == 'three' || $variants == 'four') : ?> 
 
-      		<div class="col-md-12 single-project">
-
-      		<?php endif; ?>
+	<?php endif; ?>
 
 
-
-
-			<?php 
+						
+				<div class="row project-details">
+					<div class="col-md-12">
+<?php 
 
 				while ( have_posts() ) : the_post(); 
 
 					the_content(); 
 					
 				endwhile;
-				 
-			?>	
-
-
-				<?php if ( $visible == 'show') : ?> 
-
-
-
-				<div class="project-details">
-
-				<!-- <h4><?php echo esc_html($project_title); ?></h4> -->
-
-				<h5><?php echo esc_html($author_title); ?></h5>
-					<p><?php echo esc_html($author); ?></p>
-
-				<h5><?php echo esc_html($client_title); ?></h5>
-					<p><?php echo esc_html($client_name); ?></p>
-
-				<h5><?php echo esc_html($project_date_title); ?></h5>
-					<p><?php echo esc_html($project_date); ?></p>
-
-				<h5><?php echo esc_html($project_location_title); ?></h5>
-					<p><?php echo esc_html($project_location); ?></p>
-
-<?php
-// check if the repeater field has rows of data
+				 ?> 
+						<div class="row">
+							<div class="col-md-12 project-points">
+						<?php
+						// check if the repeater field has rows of data
 						if( have_rows('project_brands') ):	
-?>
-				<p class="project-brands">
-<?php
 							$rowCount = count( get_field('project_brands')); //GET THE COUNT
+							$brandLabel = "Brand";
+							if($rowCount > 1): $brandLabel = "Brands"; endif;
+						?>
+							<div class="row">
+								<div class="col-md-3 detail-label">
+									<span><?php echo $brandLabel ?></span>
+								</div>
+				<!-- /.col-md.3 detail-label -->
+								<div class="col-md-9 detail-content">
+									<p class="project-brands">
+							<?php
 							$i = 1;
 							$commaSpace = ", ";
 							 while ( have_rows('project_brands') ) : the_row();
@@ -186,45 +96,40 @@
 									if($post_object):
 										$post = $post_object;
 										setup_postdata( $post );
-?>
-<a href="<?php the_permalink()?>"><?php echo  get_the_title(); echo $commaSpace; ?></a>
-<?php						
+								?>
+										<a href="<?php the_permalink()?>"><?php echo  get_the_title(); echo $commaSpace; ?></a>
+								<?php						
 								wp_reset_postdata();
 								endif;
 								$i++;
 							endwhile;
 ?>
-				</p>
-				<!-- /.project-brands -->
-<?php
+									</p>
+									<!-- /.project-brands -->
+								</div>
+								<!-- /.col-md-9 detail-content -->
+							</div>
+							<!-- /.row -->
+						<?php
 						endif;
+						?>							
 
-
-							if ( $social_icons_portfolio == 'show' ) : ?> 
-
-					<h5><?php esc_html_e('Share', 'glacier' ); ?></h5>
-					<?php get_template_part('template-parts/social-share'); ?>
-
-				<?php endif; ?>
-
+						</div>
+						<!-- /.col-md-12 -->
+					</div>
+					<!-- /.row.project-details -->
 				</div>
-
-				<?php endif; ?>
+				<!-- /.row .content-wrap -->
+				</div>
+				<!-- /.container -->
 
 			</div>
-			
-
-		 </div>
-
-		 <!-- NAVIGATION -->
-		 <div class="portfolio-single-nav">
-
-			<?php echo glacier_single_navigation(); ?>
-
-		 </div>
-		 <!-- END NAVIGATION -->
+			<!-- /.col-md-12 -->
 
 		</div>
+		<!-- /.row -->
+	</div>
+	<!-- .container	 -->
 
 <?php endif; ?>
 
